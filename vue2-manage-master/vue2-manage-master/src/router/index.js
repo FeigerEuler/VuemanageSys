9360a27d4b24b7b1b0ba012ab521db0e65156e1a
mhf <804941144@qq.com>
2022-06-19 16:43:19 +0800

    ok

---
+++ vue2-manage-master/vue2-manage-master/src/router/index.js
@@ -0,0 +1,72 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const login = r => require.ensure([], () => r(require('@/page/login')), 'login');
+const manage = r => require.ensure([], () => r(require('@/page/manage')), 'manage');
+const home = r => require.ensure([], () => r(require('@/page/home')), 'home');
+const addClue = r => require.ensure([], () => r(require('@/page/addClue')), 'addClue');
+const extend = r => require.ensure([], () => r(require('@/page/extend')), 'extend');
+const consultant = r => require.ensure([], () => r(require('@/page/consultant')), 'consultant');
+const technician = r => require.ensure([], () => r(require('@/page/technician')), 'technician');
+const componenter = r => require.ensure([], () => r(require('@/page/componenter')), 'componenter');
+const treasurer = r => require.ensure([], () => r(require('@/page/treasurer')), 'treasurer');
+const manager = r => require.ensure([], () => r(require('@/page/manager')), 'manager');
+
+const todoList = r => require.ensure([], () => r(require('@/page/todoList')), 'todoList');
+
+
+const routes = [
+	{
+		path: '/',
+		component: login
+	},
+	{
+		path: '/manage',
+		component: manage,
+		name: '',
+		children: [{
+			path: '',
+			component: home,
+			meta: [],
+		},{
+			path: '/addClue',
+			component: addClue,
+			meta: ['添加数据', '添加商铺'],
+		},{
+            path: '/consultant',
+            component: consultant,
+            meta: ['添加数据', '添加商铺'],
+        },{
+            path: '/technician',
+            component: technician,
+            meta: ['添加数据', '添加商铺'],
+        },{
+            path: '/componenter',
+            component: componenter,
+            meta: ['添加数据', '添加商铺'],
+        },{
+            path: '/treasurer',
+            component: treasurer,
+            meta: ['添加数据', '添加商铺'],
+        },{
+            path: '/manager',
+            component: manager,
+            meta: ['添加数据', '添加商铺'],
+        },{
+            path: '/extend',
+            component: extend,
+            meta: ['添加数据', '添加商铺'],
+        },{
+			path: '/todoList',
+			component: todoList,
+			meta: ['数据管理', '用户列表'],
+		}]
+	}
+]
+
+export default new Router({
+	routes,
+	strict: process.env.NODE_ENV !== 'production',
+})
